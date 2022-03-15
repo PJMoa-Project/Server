b5262d5584d9784b3e03e9921256014b0d29befe
choidoorim <doorim321@naver.com>
2022-03-15 23:03:53 +0900

    feat: config module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
