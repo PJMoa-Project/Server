5e31d3c8423a2d86e495be96767d4f1d97a9cd04
choidoorim <doorim321@naver.com>
2022-03-15 23:14:33 +0900

    feat: isGlobal 설정, envFilePath 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,14 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.${process.env.NODE_ENV}.env`],
+      isGlobal: true,
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
