ba5b53b5a1cef6813f3d1639732ae924d1b02256
choidoorim <doorim321@naver.com>
2022-04-02 21:38:43 +0900

    feat: typeorm 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
+const ENV = process.env;
+
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: ENV.DB_HOST,
+      port: +ENV.DB_PORT,
+      username: ENV.DB_USERNAME,
+      password: ENV.DB_PASSWORD,
+      database: ENV.DB_NAME,
+      entities: [],
+      synchronize: true,
+      logging: true,
+    }),
     ConfigModule.forRoot({
-      envFilePath: [`environments/.${process.env.NODE_ENV}.env`],
+      envFilePath: [`environments/.${ENV.NODE_ENV}.env`],
       isGlobal: true,
     }),
     UserModule,
