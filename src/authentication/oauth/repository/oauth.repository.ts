4b6f6ef889e1e98f9cc5d4b749f76019bb52008d
choidoorim <doorim321@naver.com>
2022-06-04 01:33:24 +0900

    feat: create, get oauth

--- src/authentication/oauth/repository/oauth.repository.ts
+++ src/authentication/oauth/repository/oauth.repository.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 
-import { Oauth } from '@app/entity';
+import { Oauth, OauthProviderType } from '@app/entity';
 
 @Injectable()
 @EntityRepository(Oauth)
-export class OauthRepository extends Repository<Oauth> {}
+export class OauthRepository extends Repository<Oauth> {
+  public createOauth(id: string, userId: number, provider: OauthProviderType) {
+    return this.insert({
+      id,
+      userId,
+      provider,
+    });
+  }
+
+  public getOauth(id: string) {
+    const query = this.createQueryBuilder('Oauth').where('Oauth.id = :id', {
+      id,
+    });
+
+    return query.getOne();
+  }
+}
