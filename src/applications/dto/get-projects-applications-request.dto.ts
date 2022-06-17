6c29d5c6239c28954a5b5d5ec62727dfce262911
choidoorim <doorim321@naver.com>
2022-06-18 00:44:48 +0900

    feat: 누락된 swagger property 추가

--- src/applications/dto/get-projects-applications-request.dto.ts
+++ src/applications/dto/get-projects-applications-request.dto.ts
@@ -1,7 +1,9 @@
 import { Type } from 'class-transformer';
 import { IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetProjectsApplicationsParamRequestDto {
+  @ApiProperty()
   @Type(() => Number)
   @IsNotEmpty()
   @IsNumber()
--- src/users/dto/get-user-introduce-request.dto.ts
+++ src/users/dto/get-user-introduce-request.dto.ts
@@ -1,7 +1,9 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetUserIntroduceParamRequestDto {
+  @ApiProperty()
   @Type(() => Number)
   @IsNumber()
   @IsNotEmpty()
